170454d06ec1d6b0556412b2a74262e5e9b2fda5
Dhaesha M <dhaesha1@gmail.com>
2022-05-18 21:17:07 -0400

    test failed bc its checking functionality of a contract that has not been deployed. In order to deploy ChainGang, I will create a migrations file

--- test/ChainGang.test.js
+++ test/ChainGang.test.js
@@ -2,4 +2,11 @@ const ChainGang = artifacts.require("ChainGang");
 
 contract("ChainGang", (accounts) => {
   console.log(accounts);
+
+  before(async () => {
+    chaingang = await Chaingang.deployed();
+  });
+  it("gives the owner 1m tokens", async () => {
+    let balance = await chaingang.balanceOf(accounts[0]);
+  });
 });
