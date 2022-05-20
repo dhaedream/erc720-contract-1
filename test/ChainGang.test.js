b84620e8d075a13b840863ae4e522e82602d50ff
Dhaesha M <dhaesha1@gmail.com>
2022-05-19 21:54:14 -0400

    smart contract is able to ceate tokens + trransfer them between truffle test accounts

--- test/ChainGang.test.js
+++ test/ChainGang.test.js
@@ -8,11 +8,20 @@ contract("ChainGang", (accounts) => {
   });
   it("gives the owner 1m tokens", async () => {
     let balance = await chaingang.balanceOf(accounts[0]);
-    balance = web3.utils.fromWei(balance, "etherr..!");
+    balance = web3.utils.fromWei(balance, "ether");
+    // check that balance is equal to 1000000
     assert.equal(
       balance,
       "1000000",
       "Balance should be 1M tokens for contract creator"
     );
+  });
+  it("can transfer tokens between accounts", async () => {
+    let amount = web3.utils.toWei("1000", "ether");
+    await chaingang.transfer(accounts[1], amount, { from: accounts[0] });
+
+    let balance = await chaingang.balanceOf(accounts[1]);
+    balance = web3.utils.fromWei(balance, "ether");
+    assert.equal(balance, "1000", "Balance should be 1k from contract creator");
   });
 });
